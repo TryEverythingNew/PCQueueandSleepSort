76939edb4676f4392b5eb457a5a1c80b152c80d2
xueliangsun <xueliang.sun@wsu.edu>
2016-09-08 18:29:12 -0700

    1st version of the code
    both parts are not perfect
    Part 1: PCQueue for product/consumer queue;
    Part 2: SleepSort Algo with multiple threads

---
+++ HW2_CS422/CS422/PCQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS422
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node( int value)
+        {
+            data = value;
+            next = null;
+        }
+    }
+
+    public class PCQueue
+    {
+        Node dummy;
+        Node F;
+        Node B;
+
+        public PCQueue()
+        {
+            dummy = new Node(0);
+            F = B = dummy;
+        }
+
+        public bool Dequeue(ref int out_value)
+        {
+            if(dummy.next == null)
+            {
+                return false;
+            }
+            else
+            {
+                out_value = dummy.next.data;
+                dummy.next = dummy.next.next;
+                return true;
+            }
+        }
+
+        public void Enqueue(int dataValue)
+        {
+            B.next = new Node(dataValue);
+            B = B.next;
+            if (dummy.next == null)
+            {
+                dummy.next = B;
+            }
+            
+        }
+    }
+}
---
+++ HW2_CS422/ThreadPoolSleepSorter/ThreadPoolSleepSorter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CS422
+{
+    public class ThreadPoolSleepSorter
+    {
+        TextWriter tw;
+        BlockingCollection<Task> coll;
+
+        public ThreadPoolSleepSorter(TextWriter output, ushort threadCount)
+        {
+            tw = output;
+            if( threadCount == 0)
+            {
+                for(int i = 0; i < 64; i++)
+                {
+                    var t = new Thread(ThreadWorkFunc);
+                    t.Start();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < threadCount; i++)
+                {
+                    var t = new Thread(ThreadWorkFunc);
+                    t.Start();
+                }
+            }
+        }
+
+        public void Sort(byte[] values)
+        {
+            Action<object> action = (object obj) =>
+            {
+                Thread.Sleep(((int) obj) * 1000);
+                Console.WriteLine((int)obj);
+            };
+            for(int i = 0; i < values.Length; i++)
+            {
+                Task t = new Task(action, (int) values[i]);
+                coll.Add(t);
+            }
+            
+        }
+
+        public void ThreadWorkFunc()
+        {
+            while (true)
+            {
+                coll.Take().Start();
+            }
+        }
+    }
+}
