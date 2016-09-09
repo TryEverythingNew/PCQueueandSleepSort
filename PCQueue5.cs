f1e83cfbed43b4c9aef18f345425a674f69f5720
xueliangsun <xueliang.sun@wsu.edu>
2016-09-09 01:26:17 -0700

    version new
    Previous version got 6 points:
    PCQueue basic tests: OK
    
    (-4) 37 lines in TextWriter output (expected 49 or 50)
    ThreadPoolSleepSorter portion: 0/4

---
+++ PCQueue5.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS422
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node( int value)
+        {
+            data = value;
+            next = null;
+        }
+    }
+
+    public class PCQueue
+    {
+        Node dummy;
+        Node F;
+        Node B;
+
+
+        public PCQueue()
+        {
+            dummy = new Node(0);
+            B = dummy;
+            F = B;
+        }
+
+        
+
+        public bool Dequeue(ref int out_value)
+        {
+            if (dummy.next != null)
+            {
+                if (Object.ReferenceEquals(B, dummy))
+                {
+                    return false;
+                }
+                else if (Object.ReferenceEquals(F, dummy))
+                {
+                    F = F.next;
+                    dummy.next = F.next;
+                    return false;
+                }
+                else if (Object.ReferenceEquals(F, dummy.next))
+                {
+                    out_value = F.data;
+                    dummy.next = F.next;
+                    F = dummy;
+                    return true;
+                }
+                else
+                {
+                    out_value = F.data;
+                    F = F.next;
+                    return true;
+                }
+            }
+            else
+            {
+                if (Object.ReferenceEquals(F, dummy))
+                {
+                    return false;
+                }
+                else if(F.next != null)
+                {
+                    out_value = F.data;
+                    F = F.next;
+                    return true;
+                }
+                else if (F != null)
+                {
+                    out_value = F.data;
+                    F = dummy;
+                    return true;
+                }
+                else
+                {
+                    F = dummy;
+                    return false;
+                }
+            }
+            
+        }
+
+        
+
+        public void Enqueue(int dataValue)
+        {
+            if (dummy.next == null)
+            {
+                B = dummy;
+            }
+            B.next = new Node(dataValue);
+            B = B.next;
+
+
+        }
+    }
+}
---
+++ ThreadPoolSleepSorter5.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CS422
+{
+    public class ThreadPoolSleepSorter : IDisposable
+    {
+        TextWriter tw;
+        BlockingCollection<Task> coll;
+        bool running, isReady;
+        Thread[] threads;
+        int num_availthread;
+
+        public ThreadPoolSleepSorter(TextWriter output, ushort threadCount)
+        {
+            
+            running = true;
+            isReady = false;
+
+            tw = output;
+            if (threadCount == 0)
+            {
+                coll = new BlockingCollection<Task>(64);
+                threads = new Thread[64];
+                for (int i = 0; i < 64; i++)
+                {
+                    threads[i] = new Thread(ThreadWorkFunc);
+                    threads[i].Start();
+                    while (!threads[i].IsAlive)
+                    {
+                    }
+                }
+            }
+            else
+            {
+                coll = new BlockingCollection<Task>(threadCount);
+                threads = new Thread[threadCount];
+                for (int i = 0; i < threadCount; i++)
+                {
+                    threads[i] = new Thread(ThreadWorkFunc);
+                    threads[i].Start();
+                    while (!threads[i].IsAlive)
+                    {
+                    }
+                }
+            }
+            num_availthread = threads.Length;
+
+
+        }
+
+        public void Sort(byte[] values)
+        {
+            while ((coll.Count != 0) || (num_availthread != threads.Length))
+            {
+            }
+            isReady = false;
+            Action<object> action = (object obj) =>
+            {
+                //Console.WriteLine(1000*(int)obj);
+                Thread.Sleep(50*(int)obj );
+
+                tw.WriteLine((int)obj);
+            };
+            
+            for (int i = 0; i < values.Length; i++)
+            {
+                coll.Add(new Task(action, (int)values[i]));
+            }
+
+            isReady = true;
+            Thread.Sleep(2000);
+        }
+
+        public void ThreadWorkFunc()
+        {
+            while (running)
+            {
+                if (isReady)
+                {
+                    Task t = null;
+                    while (!coll.TryTake(out t))
+                    {
+
+                    }
+                    num_availthread--;
+                    t.Start();
+                    num_availthread++;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            running = false;
+            isReady = false;
+        }
+    }
+}
